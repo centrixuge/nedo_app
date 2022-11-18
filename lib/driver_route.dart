94e78052c3ea0b3bade5549f392c14f92ecc6f6d
yH3PO4 <55231468+yH3PO4@users.noreply.github.com>
2022-11-18 21:53:49 +0900

    Static Map #1

--- lib/driver_route.dart
+++ lib/driver_route.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DriverRoute extends StatefulWidget {
   const DriverRoute(this.user);
@@ -23,13 +25,50 @@ class _DriverRoutePageState extends State<DriverRoute> {
       appBar: AppBar(
         title: const Text("乗車予定"),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Text('Hello World')
-          ],
-        ),
+      body: Center(
+        child: SizedBox(
+            width: double.infinity,
+            height: 150,
+            child: Image.network(
+              getStaticImageWithMarker(
+                  latitude: 35.55,
+                  longitude: 134.44,
+                  width: MediaQuery.of(context).size.width.toInt(),
+                  height: 150,
+                  zoom: 13),
+            )),
       ),
     );
   }
+}
+
+String getStaticImageWithMarker(
+    {required double longitude,
+    required double latitude,
+    required int width,
+    required int height,
+    int zoom = 16}) {
+  Map geoJson = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": '#ff0000',
+          "marker-size": 'large',
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [longitude, latitude]
+        }
+      }
+    ]
+  };
+
+  var mapboxPublicToken = dotenv.env['MAPBOX_PUBLIC_TOKEN'];
+
+  return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/'
+      'geojson(${Uri.encodeComponent(jsonEncode(geoJson))})'
+      '/$longitude,$latitude,$zoom'
+      '/${width}x$height?access_token=$mapboxPublicToken';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 // widgetツリー
@@ -14,6 +15,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
 }
 

650592fb14c1111cf06e7bda5dc5d46217e4af87
centrixuge <raionpanda@gmail.com>
2022-11-18 17:55:08 +0900

    added firebase_options.dart

---
+++ ppsupply/ppsupply/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ppsupply project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ppsupply.settings')
+
+application = get_wsgi_application()
